9ef532efb336743951c615faacbdac0585bb4574
Ahmad Ashraf <ahmadashraf159@gmail.com>
2019-10-09 10:32:08 +0300

    Add checkout page, cart section, html pages for the product categories and the payment page

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $(".from").click(function () {
+        $('html, body').animate({
+            scrollTop: $(".household").offset().top
+        }, 1500);
+    });
+
+    $(".icon_box").click(function () {
+        $(".icon_box_image").fadeToggle();
+        $(".lead").fadeToggle();
+    });
+    $(".icon_box1").click(function () {
+        $(".icon_box_image1").fadeToggle();
+        $(".lead1").fadeToggle();
+    });
+    $(".icon_box2").click(function () {
+        $(".icon_box_image2").fadeToggle();
+        $(".lead2").fadeToggle();
+    });
+    $(".categ").click(function () {
+        $(".household-items1, .bed, .window, .linen").fadeToggle();
+        $(".household").fadeToggle();
+    });
+});
